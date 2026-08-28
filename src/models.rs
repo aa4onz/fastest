f09737325fb082e519f5e6112787cd5de68900d4
aa4onz <aa4onz@gmail.com>
2026-08-28 07:29:26 -0400

    Update models.rs

--- src/models.rs
+++ src/models.rs
@@ -16,6 +16,7 @@ pub struct DiscordMessage {
 #[derive(Debug, Clone)]
 pub enum AppEvent {
     IncomingMessage(DiscordMessage),
+    UserTyping { username: String, channel_id: String },
     MessageSent { nonce: String, timestamp: String },
     MessageFailed { nonce: String },
     Terminal(crossterm::event::Event),
